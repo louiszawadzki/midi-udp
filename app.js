98e7a08b156692bb487d0dd005345a74b8e089ca
Louis <louis.zawadzki@ecl2015.ec-lyon.fr>
2016-02-22 17:56:03 +0100

    sending arraybuffer through UDP

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
 var appStatus = document.getElementById("status");
+
 // Getting MIDI
+var socketId;
+
+var onMIDIMessage = function(message){
+  var buffer = message.data.buffer;
+  chrome.sockets.udp.send(socketId, buffer, '127.0.0.1', 1337, function(sendInfo){
+    console.log("sending info through udp" + buffer);
+  });
+};
+var onMIDISuccess = function(midiAccess){
+  var inputs = midiAccess.inputs.values();
+  for (var input = inputs.next(); input && !input.done; input= inputs.next()){
+    console.log("new input " + input);
+    input.value.onmidimessage = onMIDIMessage;
+  };
+};
+var onMIDIFailure = function(e){
+  console.log("fail" + e);
+};
+if (navigator.requestMIDIAccess) {
+    console.log("h");
+    navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
+} else {
+    console.log("No MIDI support in your browser.");
+}
 
 // UDP Connection
 var properties = {};
 chrome.sockets.udp.create(properties, function(createInfo){
-  var socketId = createInfo.socketId;
+  socketId = createInfo.socketId;
   appStatus.innerHTML = "socket created";
   chrome.sockets.udp.onReceive.addListener(function(data){
     console.log(data);
   });
   chrome.sockets.udp.bind(socketId, '0.0.0.0', 0, function(r){
     appStatus.innerHTML = "socket " + socketId + " bound to port";
-    /*chrome.sockets.udp.send(socketId, arrayBuffer, '127.0.0.1', 1337, function(sendInfo){
-      console.log("sending info through udp");
-    });*/
   });
 });
+
